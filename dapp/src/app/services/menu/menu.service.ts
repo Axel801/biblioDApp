f4dd3fcded57b1a89cfc239b2d46d9ca76711ece
Axel801 <axelloupias@gmail.com>
2022-02-05 16:14:29 +0100

    Fix url

--- dapp/src/app/services/menu/menu.service.ts
+++ dapp/src/app/services/menu/menu.service.ts
@@ -17,8 +17,8 @@ export class MenuService {
       {
         title: 'Alquilar',
         icon: 'book',
-        link: '',
-        url: '/',
+        link: '/',
+        // url: '/',
       },
       {
         title: 'Mios',
